7cb519515a43bff5f2e0b3fbfb070f2122ca3265
AliHaine <alitr57120@gmail.com>
2025-09-05 03:19:13 +0200

    The load command now correctly checks and generates a world according to its type (Overworld, Nether, or End).

--- src/main/java/com/alihaine/bulmultiverse/command/LoadCommand.java
+++ src/main/java/com/alihaine/bulmultiverse/command/LoadCommand.java
@@ -5,8 +5,11 @@ import co.aikar.commands.annotation.*;
 import com.alihaine.bulmultiverse.BulMultiverse;
 import com.alihaine.bulmultiverse.file.Message;
 import com.alihaine.bulmultiverse.world.WorldData;
+import com.alihaine.bulmultiverse.world.WorldOption;
+import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 
+import java.io.File;
 import java.util.HashMap;
 
 @CommandAlias(BaseBmvCommand.commandRootAlias)
@@ -15,12 +18,20 @@ public class LoadCommand extends BaseCommand {
     @Subcommand("load")
     @Description("Load existing world")
     @Syntax("/bmv load [world_name]")
-    public void onLoad(CommandSender sender, String targetWorld) {
+    public void onLoad(CommandSender sender, String targetWorld) throws Exception {
         if (!BulMultiverse.getWorldsFile().isWorldFolderExisting(targetWorld)) {
             new Message("world_not_found").withPlaceHolder("name", targetWorld).sendMessage(sender);
             return;
         }
-        WorldData worldData = new WorldData(targetWorld, new HashMap<>());
+
+        final HashMap<WorldOption, Object> options = new HashMap<>();
+        File file = new File(Bukkit.getWorldContainer(), targetWorld);
+        if (new File(file, "DIM-1").exists())
+            options.put(BulMultiverse.getWorldOptionManager().getOption("-e"), "nether");
+        else if (new File(file, "DIM1").exists())
+            options.put(BulMultiverse.getWorldOptionManager().getOption("-e"), "the_end");
+
+        WorldData worldData = new WorldData(targetWorld, options);
         worldData.createWorld(sender);
         BulMultiverse.getWorldsFile().saveWorldDataToFile(worldData);
     }
